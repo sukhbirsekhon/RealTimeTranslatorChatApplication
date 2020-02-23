3e47e7439c699f8addd873968b394f6a2a764a5e
sekhonss <>
2020-02-23 02:34:36 -0500

    :confetti_ball:

---
+++ realtimechatapp/assets/app/chat.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from "@angular/core";
+import * as io from 'socket.io-client';
+import {Observable} from 'rxjs/Observable';
+
+@Injectable()
+
+export class ChatService{
+
+    private socket = io('http://localhost:3000');
+
+    joinRoom(data)
+    {
+        this.socket.emit('join',data);
+    }
+
+    choseLanguage(data)
+    {
+        this.socket.emit('lang', data);
+    }
+    newUserJoined()
+    {
+        let observable = new Observable<{user:String, message:String}>(observer=>{
+            this.socket.on('new user joined', (data)=>{
+                observer.next(data);
+            });
+            return () => {this.socket.disconnect();}
+        });
+
+        return observable;
+    }
+
+    leaveRoom(data){
+        this.socket.emit('leave',data);
+    }
+
+    userLeftRoom(){
+        let observable = new Observable<{user:String, message:String}>(observer=>{
+            this.socket.on('left room', (data)=>{
+                observer.next(data);
+            });
+            return () => {this.socket.disconnect();}
+        });
+
+        return observable;
+    }
+
+    sendMessage(data)
+    {
+        this.socket.emit('message',data);
+        this.socket.emit('lang', data);
+    }
+
+    newMessageReceived(){
+        let observable = new Observable<{user:String, message:String}>(observer=>{
+            this.socket.on('new message', (data)=>{
+                observer.next(data);
+            });
+            return () => {this.socket.disconnect();}
+        });
+
+        return observable;
+    }
+}
